b75f5a29388eae060977534e1fb9e65bc2a2ecf8
BaileyV <baileyvarness@gmail.com>
2019-11-11 16:44:03 -0800

    first commit

--- apps/semi_restful_app/models.py
+++ apps/semi_restful_app/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class ShowsManager(models.Manager):
+    def validator(self, post_data):
+        print('post_data: ', post_data)
+        errors = {}
+        if len(post_data['title']) < 2:
+            print('title is too short')
+            errors['title'] = "title is too short"
+        if len(post_data['network']) < 3:
+            print('network is too short')
+            errors['network'] = "network is too short"
+        if len(post_data['description']) < 10:
+            print('description is too short')
+            errors['description'] = "description is too short"
+        
+        return errors
+
+
 class Shows(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
@@ -8,3 +24,4 @@ class Shows(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowsManager()
--- apps/semi_restful_app/views.py
+++ apps/semi_restful_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import *
+from django.contrib import messages
 
 def shows_new(request):
     context= {
@@ -9,7 +10,14 @@ def shows_new(request):
 
 def add_show(request):
     print("Someone added a show")
-    new_show = Shows.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
+    errors = Shows.objects.validator(request.POST)
+    print('errors', errors)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/new")
+    else:
+        new_show = Shows.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
     return redirect(f"/show_page/{new_show.id}")
 
 def show_page(request, show_id):
@@ -33,12 +41,18 @@ def show_edit(request, show_id):
 
 def edit_show(request, show_id):
     print("Someone edited a show")
-    show = Shows.objects.get(id=show_id)
-    show.title=request.POST['title'] 
-    show.network=request.POST['network']
-    show.release_date=request.POST['release_date']
-    show.description=request.POST['description']
-    show.save()
+    errors = Shows.objects.validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f"/shows/{show_id}/edit")
+    else:
+        show = Shows.objects.get(id=show_id)
+        show.title=request.POST['title'] 
+        show.network=request.POST['network']
+        show.release_date=request.POST['release_date']
+        show.description=request.POST['description']
+        show.save()
     return redirect(f"/show_page/{show_id}")
 
 
